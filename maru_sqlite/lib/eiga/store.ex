c44e63c1ce371da06527e81b6cb7986fa2455952
defndaines <defndaines@gmail.com>
2017-01-03 20:02:06 -0800

    Formatting clean up

--- maru_sqlite/lib/eiga/store.ex
+++ maru_sqlite/lib/eiga/store.ex
@@ -9,7 +9,7 @@ defmodule Eiga.Store do
   alias Eiga.Movie
   alias Eiga.Review
 
-  @doc "Get a list of all movies I have watched."
+  @doc "Get a list of all movies watched."
   def all_movies do
     Movie
     |> select([movie], movie)
@@ -47,52 +47,52 @@ defmodule Eiga.Store do
   @doc "Get a list of all reviews."
   def all_reviews do
     query = from r in Review,
-    join: m in Movie,
-    order_by: r.view_date,
-    select: %{movie: m.title, year: m.year, country: m.country,
-      location: r.location, view_date: r.view_date, review: r.text}
-      Repo.all(query)
-      end
+            join: m in Movie,
+            order_by: r.view_date,
+            select: %{movie: m.title, year: m.year, country: m.country,
+              location: r.location, view_date: r.view_date, review: r.text}
+    Repo.all(query)
+  end
 
-      @doc """
-      Insert a movie, but only if it doesn't exist in the database yet.
-      Assumes that there is only one movie per year with a given name.
-      """
-      def insert_movie(%{"title" => title, "short_title" => short_title, "year" => year, "country" => country}) do
-        date = String.to_integer(year)
-        case Repo.get_by(Movie, %{title: title, year: date}) do
-          nil ->
-            {:ok, new_movie} = Repo.insert(%Movie{title: title, short_title: short_title, year: date, country: country},
-                                           on_conflict: :ignore, conflict_taget: [:title, :year])
-            new_movie
-          existing -> existing
-        end
-      end
+  @doc """
+  Insert a movie, but only if it doesn't exist in the database yet.
+  Assumes that there is only one movie per year with a given name.
+  """
+  def insert_movie(%{"title" => title, "short_title" => short_title, "year" => year, "country" => country}) do
+    date = String.to_integer(year)
+    case Repo.get_by(Movie, %{title: title, year: date}) do
+      nil ->
+        {:ok, new_movie} = Repo.insert(%Movie{title: title, short_title: short_title, year: date, country: country},
+                                       on_conflict: :ignore, conflict_taget: [:title, :year])
+        new_movie
+      existing -> existing
+    end
+  end
 
-      @doc """
-      Insert a movie review, but only if it doesn't exist in the database yet.
-      Assumes that there is only one review per movie on a given date.
-      """
-      def insert_review(%{"location" => location, "short_title" => short_title, "text": text, "view_date": view_date}) do
-        {:ok, date} = Ecto.Date.cast(view_date)
-        # Movie must already be in the DB.
-        movie = Repo.get_by!(Movie, short_title: short_title)
-        case Repo.get_by(Review, %{movie_id: movie.id, view_date: date}) do
-          nil ->
-            {:ok, new_review} = Repo.insert(%Review{movie_id: movie.id, location: location, view_date: date, text: text},
-                                            on_confict: :ignore, conflict_target: [:movie_id, :view_date])
-            new_review
-          existing -> existing
-        end
-      end
-      def insert_review(got) do  ## FIXME: What is wrong with the function clause above?!? This gets called by data_import
-        {:ok, date} = Ecto.Date.cast(got["view_date"])
-        movie = Repo.get_by!(Movie, short_title: got["short_title"])
-        case Repo.get_by(Review, %{movie_id: movie.id, view_date: date}) do
-          nil ->
-            {:ok, new_review} = Repo.insert(%Review{movie_id: movie.id, location: got["location"], view_date: date, text: got["text"]})
-            new_review
-          existing -> existing
-        end
-      end
+  @doc """
+  Insert a movie review, but only if it doesn't exist in the database yet.
+  Assumes that there is only one review per movie on a given date.
+  """
+  def insert_review(%{"location" => location, "short_title" => short_title, "text": text, "view_date": view_date}) do
+    {:ok, date} = Ecto.Date.cast(view_date)
+    # Movie must already be in the DB.
+    movie = Repo.get_by!(Movie, short_title: short_title)
+    case Repo.get_by(Review, %{movie_id: movie.id, view_date: date}) do
+      nil ->
+        {:ok, new_review} = Repo.insert(%Review{movie_id: movie.id, location: location, view_date: date, text: text},
+                                        on_confict: :ignore, conflict_target: [:movie_id, :view_date])
+        new_review
+      existing -> existing
+    end
+  end
+  def insert_review(got) do  ## FIXME: What is wrong with the function clause above?!? This gets called by data_import
+    {:ok, date} = Ecto.Date.cast(got["view_date"])
+    movie = Repo.get_by!(Movie, short_title: got["short_title"])
+    case Repo.get_by(Review, %{movie_id: movie.id, view_date: date}) do
+      nil ->
+        {:ok, new_review} = Repo.insert(%Review{movie_id: movie.id, location: got["location"], view_date: date, text: got["text"]})
+        new_review
+      existing -> existing
+    end
+  end
 end
