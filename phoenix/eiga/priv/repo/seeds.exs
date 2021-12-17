ece40aba04ce74d239b1e12ceeec6aebebc7551a
Michael Daines <michael.daines@producepay.com>
2021-12-17 14:46:09 -0800

    Add movie.dat as seed data

--- phoenix/eiga/priv/repo/seeds.exs
+++ phoenix/eiga/priv/repo/seeds.exs
@@ -9,3 +9,16 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+File.stream!("../../sql/movie.dat")
+|> Enum.each(fn line ->
+  [short_title, title, year, country] = line |> String.trim() |> String.split("|")
+
+  %Eiga.Schema.Movie{
+    short_title: short_title,
+    title: title,
+    year: String.to_integer(year),
+    country: country
+  }
+  |> Eiga.Repo.insert!(on_conflict: :nothing)
+end)
