62adf1277572153982c7d7aa22e5eaabb8fecaea
Michael Daines <michael.daines@producepay.com>
2021-12-21 07:39:09 -0800

    Allow for cross-referencing queries: reviews and movies

--- phoenix/eiga/lib/eiga/movies.ex
+++ phoenix/eiga/lib/eiga/movies.ex
@@ -14,7 +14,22 @@ defmodule Eiga.Movies do
   """
   def list_movies, do: Repo.all(Movie)
 
+  @doc """
+  Returns the list of all reviews.
+  """
+  def list_reviews, do: Repo.all(Review)
+
+  def list_reviews(%{short_title: short_title}) do
+    Repo.all(from Review, where: [short_title: ^short_title])
+  end
+
+  def list_reviews(_args), do: list_reviews()
+
   def get_movie!(id), do: Repo.get!(Movie, id)
 
-  def get_movie_by_short_title!(short_title), do: Repo.get_by!(Movie, short_title: short_title)
+  def get_movie_by_short_title(short_title), do: Repo.get_by(Movie, short_title: short_title)
+
+  def get_review_by_short_title_and_view_date(short_title, view_date) do
+    Repo.get_by(Review, short_title: short_title, view_date: view_date)
+  end
 end
--- phoenix/eiga/lib/eiga_web/schema.ex
+++ phoenix/eiga/lib/eiga_web/schema.ex
@@ -13,6 +13,9 @@ defmodule EigaWeb.Schema do
     field :title, non_null(:string)
     field :year, non_null(:integer)
     field :country, non_null(:string)
+    field :reviews, list_of(:review) do
+      resolve(&Movies.all_reviews/3)
+    end
   end
 
   object :review do
@@ -22,12 +25,32 @@ defmodule EigaWeb.Schema do
     # TODO: Date as string for now. Adjust later?
     field :view_date, non_null(:string)
     field :text, non_null(:string)
+    field :movie, :movie do
+      resolve(&Movies.find_movie/3)
+    end
   end
 
   query do
     @desc "Get all movies"
     field :all_movies, non_null(list_of(non_null(:movie))) do
       resolve(&Movies.all_movies/3)
+    end
+
+    field :movie, :movie do
+      arg(:short_title, non_null(:string))
+      resolve(&Movies.find_movie/3)
+    end
+
+    @desc "Get all reviews"
+    field :reviews, list_of(:review) do
+      arg(:short_title, :string)
+      resolve(&Movies.all_reviews/3)
+    end
+
+    field :review, :review do
+      arg(:short_title, non_null(:string))
+      arg(:view_date, non_null(:string))
+      resolve(&Movies.find_review/3)
     end
   end
 end
